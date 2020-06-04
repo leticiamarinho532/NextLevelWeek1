0b60c54103fad395ff6374afa323302b8d6cb5f3
Leticia <leticia.marinho532@gmail.com>
2020-06-04 02:44:50 -0300

    rotas de listagem de itens e criação de ponto de coleta

--- server/knexfile.ts
+++ server/knexfile.ts
@@ -10,7 +10,10 @@ module.exports = {
     migrations: {
       directory: path.resolve(__dirname, "src", "database", "migrations")
     },
-    useNullAsDefault:true
+    seeds: {
+      directory: path.resolve(__dirname, "src", "database", "seeds")
+    },
+    useNullAsDefault: true
   },
 
   // staging: {
--- server/src/routes.ts
+++ server/src/routes.ts
@@ -1,9 +1,15 @@
 import express from 'express';
+import knex from './database/connetion';
+
+import PointsController from './controllers/pointsController';
+import ItemsController from './controllers/itemsController';
 
 const routes = express.Router();
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
-routes.get('/', (request, response) => {
-    return response.json({ message: 'Hello World' });
-});
+routes.get('/items', itemsController.index);
+
+routes.post('/points', pointsController.create);
 
 export default routes;
